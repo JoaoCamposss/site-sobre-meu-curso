15bc252ea974f33148fad2c14a6ac736a583f4c8
JoaoCamposss <131219433+JoaoCamposss@users.noreply.github.com>
2024-05-13 22:30:10 -0300

    Add files via upload

---
+++ programa curso campos/script.js
@@ -0,0 +1,13 @@
+// Script para rolagem suave ao clicar nos links de navegação
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const target = document.querySelector(this.getAttribute('href'));
+
+        window.scrollTo({
+            top: target.offsetTop,
+            behavior: 'smooth'
+        });
+    });
+});
